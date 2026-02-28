c01541e36caa4a032c1c1bf7ab3845011cef6aee
Diogo Gavazzi <gavazzi19112021@gmail.com>
2026-02-28 15:43:38 +0000

    alteracoes da navbar

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $mensagem = $_POST["mensagem"];
+
+    $para = "teuemail@gmail.com"; // 🔴 MUDA PARA O TEU EMAIL
+    $assunto = "Nova mensagem do site";
+
+    $corpo = "Nome: $nome\n";
+    $corpo .= "Email: $email\n";
+    $corpo .= "Telefone: $telefone\n\n";
+    $corpo .= "Mensagem:\n$mensagem";
+
+    $headers = "From: $email";
+
+    if (mail($para, $assunto, $corpo, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar mensagem.";
+    }
+}
+?>
